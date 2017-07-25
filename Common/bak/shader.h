2d1fe81759afc542cd2367f14edc9d73961d2908
Christian Schweizer <christian@schweizerweb.ch>
2017-07-25 14:52:11 +0200

    old files removed

--- Common/bak/shader.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHADER_HPP
-#define SHADER_HPP
-#include "JuceHeader.h"
-
-GLuint LoadShaders(OpenGLContext context, const char * vertex_file_path,const char * fragment_file_path);
-
-#endif
--- Common/bak/text2D.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEXT2D_HPP
-#define TEXT2D_HPP
-#include "JuceHeader.h"
-
-void initText2D(OpenGLContext context, const char * texturePath);
-void printText2D(OpenGLContext context, const char * text, int x, int y, int size);
-void cleanupText2D(OpenGLContext context);
-
-#endif
