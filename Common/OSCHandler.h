b8987d596e9cf743a1285a586ccc16c822fb9d35
Christian Schweizer <christian@schweizerweb.ch>
2017-12-28 20:44:13 +0100

    osc handling for multiple styles

--- Common/OSCHandler.h
+++ Common/OSCHandler.h
@@ -14,11 +14,18 @@
 #include "JuceHeader.h"
 #include "../../Common/AmbiPoint.h"
 
+#define OSC_ADDRESS_MUSESCORE_SSMN "/aed"
+#define OSC_ADDRESS_ZHDK_AMBISONIC_PLUGINS "/zhdk/ambi/source/internal"
+#define OSC_ADDRESS_ZHDK_AMBISONIC_PLUGINS_EXTERN "/zhdk/ambi/source"
+
 class OSCHandler : OSCReceiver, OSCReceiver::Listener<OSCReceiver::MessageLoopCallback>
 {
 public:
 	OSCHandler(Array<AmbiPoint>* pAmbiPointArray);
 	bool start(int portNb);
+	void HandleMusescoreSSMNStyle(const OSCMessage& message) const;
+	void HandleOwnInternalStyle(const OSCMessage& message) const;
+	void HandleOwnExternStyle(const OSCMessage& message) const;
 	void oscMessageReceived(const OSCMessage& message) override;
 	void stop();
 
