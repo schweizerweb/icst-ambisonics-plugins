1d5f0262db575e30cfe1df8b52be9aa815dd8328
Christian Schweizer <christian@schweizerweb.ch>
2018-04-01 13:30:26 +0200

    using msbuild instead of devenv for windows build

--- WindowsBuild.ps1
+++ WindowsBuild.ps1
@@ -1,8 +1,8 @@
 $powershellCommand = "powershell"
 $projucer = "./../../JUCE/Projucer"
-$buildExecutable = """c:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.com"""
+$buildExecutable = """c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"""
 $buildArgumentsPre = ""
-$buildArgumentsPost = "/build ""Release"""
+$buildArgumentsPost = "/p:Configuration=Release"
 $projectFileExtension = "*.sln"
 $platformString = "win_x64"
 $pluginTypeStrings = @("VST;*.dll", "VST3;*.vst3")
