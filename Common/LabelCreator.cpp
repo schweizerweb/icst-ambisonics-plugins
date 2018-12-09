049e5f26e31c521bcf7dae69512afd68b27d6abe
Christian Schweizer <christian@schweizerweb.ch>
2018-12-09 09:31:48 +0100

    explicit int conversion

--- Common/LabelCreator.cpp
+++ Common/LabelCreator.cpp
@@ -18,11 +18,11 @@ Image* LabelCreator::createNewLabel(String label, Colour newColor, float fontSiz
 	if (width <= 0)
 		return new Image();
 
-	Image* img = new Image(Image::ARGB, width, fontSize, true);
+	Image* img = new Image(Image::ARGB, width, int(fontSize+0.5), true);
 	Graphics g(*img);
 	g.setColour(newColor);
 	g.setFont(fontSize);
-	g.drawSingleLineText(label, 0, fontSize);
+	g.drawSingleLineText(label, 0, int(fontSize+0.5));
 	return img;
 }
 
