556b1631095ee10449f7092756795f770c9cc8e9
Christian Schweizer <christian@schweizerweb.ch>
2019-11-20 14:44:28 +0100

    windows installer added

--- WindowsBuild.ps1
+++ WindowsBuild.ps1
@@ -6,5 +6,6 @@ $buildArgumentsPost = "/p:Configuration=Release"
 $projectFileExtension = "*.sln"
 $platformString = "win_x64"
 $pluginTypeStrings = @("VST3;*.vst3")
+$buildInstallScript = "Installer_Win64.ps1"
 
-. .\BuildScript.ps1 $powershellCommand $projucer $buildExecutable $buildArgumentsPre $buildArgumentsPost $projectFileExtension $platformString $pluginTypeStrings
+. .\BuildScript.ps1 $powershellCommand $projucer $buildExecutable $buildArgumentsPre $buildArgumentsPost $buildInstallScript $projectFileExtension $platformString $pluginTypeStrings
