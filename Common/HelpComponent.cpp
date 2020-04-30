0d1b0585e30311b43667582d9084e97edbe692da
Christian Schweizer <christian@schweizerweb.ch>
2020-04-30 16:57:15 +0200

    copyright label added

--- Common/HelpComponent.cpp
+++ Common/HelpComponent.cpp
@@ -41,6 +41,15 @@ HelpComponent::HelpComponent (bool isEncoder)
     tabHelp->addTab (TRANS("Radar Interaction"), Colours::lightgrey, new HelpRadarInteraction(), true);
     tabHelp->setCurrentTabIndex (0);
 
+    label.reset (new Label ("new label",
+                            CharPointer_UTF8 ("\xc2\xa9 2020 Johannes Schuett & Christian Schweizer @ ICST")));
+    addAndMakeVisible (label.get());
+    label->setFont (Font (15.00f, Font::plain).withTypefaceStyle ("Regular"));
+    label->setJustificationType (Justification::centred);
+    label->setEditable (false, false, false);
+    label->setColour (TextEditor::textColourId, Colours::black);
+    label->setColour (TextEditor::backgroundColourId, Colour (0x00000000));
+
 
     //[UserPreSize]
     if(isEncoder)
@@ -70,6 +79,7 @@ HelpComponent::~HelpComponent()
     //[/Destructor_pre]
 
     tabHelp = nullptr;
+    label = nullptr;
 
 
     //[Destructor]. You can add your own custom destruction code here..
@@ -93,7 +103,8 @@ void HelpComponent::resized()
     //[UserPreResize] Add your own custom resize code here..
     //[/UserPreResize]
 
-    tabHelp->setBounds (0, 0, getWidth() - 0, getHeight() - 0);
+    tabHelp->setBounds (0, 0, getWidth() - 0, getHeight() - 22);
+    label->setBounds (0, getHeight() - 3 - 16, getWidth() - 0, 16);
     //[UserResized] Add your own custom resize handling here..
     //[/UserResized]
 }
@@ -119,11 +130,17 @@ BEGIN_JUCER_METADATA
                  overlayOpacity="0.330" fixedSize="0" initialWidth="600" initialHeight="400">
   <BACKGROUND backgroundColour="ff323e44"/>
   <TABBEDCOMPONENT name="tabHelp" id="7fe78fa9ebb609cc" memberName="tabHelp" virtualName=""
-                   explicitFocusOrder="0" pos="0 0 0M 0M" orientation="top" tabBarDepth="30"
+                   explicitFocusOrder="0" pos="0 0 0M 22M" orientation="top" tabBarDepth="30"
                    initialTab="0">
     <TAB name="Radar Interaction" colour="ffd3d3d3" useJucerComp="0" contentClassName="HelpRadarInteraction"
          constructorParams="" jucerComponentFile=""/>
   </TABBEDCOMPONENT>
+  <LABEL name="new label" id="c9e7e52a2e8c0879" memberName="label" virtualName=""
+         explicitFocusOrder="0" pos="0 3Rr 0M 16" edTextCol="ff000000"
+         edBkgCol="0" labelText="&#169; 2020 Johannes Schuett &amp; Christian Schweizer @ ICST"
+         editableSingleClick="0" editableDoubleClick="0" focusDiscardsChanges="0"
+         fontname="Default font" fontsize="15.0" kerning="0.0" bold="0"
+         italic="0" justification="36"/>
 </JUCER_COMPONENT>
 
 END_JUCER_METADATA
