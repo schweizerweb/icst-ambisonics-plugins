6875f31581e46e18c474c0afff16f89047360eb3
Christian Schweizer <christian@schweizerweb.ch>
2020-08-13 00:06:52 +0200

    nullptr handling, gesture start/stop prepared

--- Common/AudioParameterSet.h
+++ Common/AudioParameterSet.h
@@ -42,8 +42,22 @@ public:
     
     void setEnabled(bool enable)
     {
-        pX->setEnabled(enable);
-        pY->setEnabled(enable);
-        pZ->setEnabled(enable);
+        if(pX != nullptr) pX->setEnabled(enable);
+        if(pY != nullptr) pY->setEnabled(enable);
+        if(pZ != nullptr) pZ->setEnabled(enable);
     }
+
+    void notifyStartGesture() const
+    {
+        if (pX != nullptr) pX->beginChangeGesture();
+        if (pY != nullptr) pY->beginChangeGesture();
+        if (pZ != nullptr) pZ->beginChangeGesture();
+	}
+
+    void notifyEndGesture() const
+	{
+        if (pX != nullptr) pX->endChangeGesture();
+        if (pY != nullptr) pY->endChangeGesture();
+        if (pZ != nullptr) pZ->endChangeGesture();
+	}
 };
