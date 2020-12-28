87734afffae2e8b82a8126f601004e2fa8074b46
Christian Schweizer <christian@schweizerweb.ch>
2020-12-28 22:00:48 +0100

    AP-14: preset generator

--- Decoder/Source/DecoderPresetHelper.h
+++ Decoder/Source/DecoderPresetHelper.h
@@ -20,7 +20,16 @@ public:
     DecoderPresetHelper(File presetDirectory, ActionListener* pActionListener) : PresetHelper(presetDirectory, pActionListener)
     {
     }
-    
+
+    struct SSpeaker
+    {
+        double x;
+        double y;
+        double z;
+        String name;
+        bool isSubwoofer = false;
+    };
+
     bool checkValid(juce::File presetFile) override
     {
         AmbiSpeakerSet testSet;
@@ -40,8 +49,22 @@ public:
         createPreset("Hexagon (6)", 6);
         createPreset("Octagon (8)", 8);
         createPreset("Hexadecagon (16)", 16);
-        createPreset("Triacontakaidigon (32)", 32);
-        
+
+        createPreset("3D-Hexagon", { 
+            {-0.5000000000000002, 0.8660253882408143, 0.0, "1"},
+            { 0.5000000000000002, 0.8660253882408143, 0.0, "2"},
+            { 0.9999999999999999, -5.960464477539062e-8, 0.0, "3"},
+            {0.4999999105930329, -0.8660254478454589, 0.0, "4"},
+            {-0.5000001192092894, -0.8660253286361694, 0.0, "5"},
+            {-0.9999999999999999, 1.788139343261719e-7, 0.0, "6"},
+            {0.0, 0.7071067811865475, 0.7071067811865475, "7"},
+            {0.6123724356957946, 0.3535533905932738, 0.7071067811865475, "8"},
+            {0.6123724356957947, -0.3535533905932736, 0.7071067811865475, "9"},
+            {8.659560562354934e-17, -0.7071067811865475, 0.7071067811865475, "10"},
+            {-0.6123724356957939, -0.3535533905932747, 0.7071067811865475, "11"},
+            {-0.6123724356957945, 0.3535533905932737, 0.7071067811865475, "12"}
+        });
+
         notifyPresetListChanged();
     }
     
@@ -119,6 +142,29 @@ public:
             {
                 speakerSet.addNew(Uuid().toString(), Point3D<double>(projectedPoint.getX(), projectedPoint.getY(), 0.0), String(i + 1), TrackColors::getSpeakerColor());
                 projectedPoint = projectedPoint.rotatedAboutOrigin(-float(PI * 2 / numberOfSpeakers));
+            }
+        }
+        
+        writeToXmlFile(file, &speakerSet, &defaultAmbiSettings);
+        presetFiles.addIfNotAlreadyThere(file);
+    }
+
+    void createPreset(String name, std::initializer_list<SSpeaker> speakerList)
+    {
+        AmbiSettings defaultAmbiSettings = getDefaultAmbiSettings();
+        AmbiSpeakerSet speakerSet;
+
+        File file = getPathForPresetName(name);
+
+        for (const SSpeaker speaker : speakerList)
+        {
+            speakerSet.addNew(Uuid().toString(), Point3D<double>(speaker.x, speaker.y, speaker.z), speaker.name, TrackColors::getSpeakerColor());
+            if(speaker.isSubwoofer)
+            {
+                FilterInfo* s = speakerSet.get(speakerSet.size() - 1)->getFilterInfo()->get(0);
+                s->filterType = FilterInfo::LowPass;
+                s->cutOffFrequencyHz = 50;
+                s->qValue = 0.8165930509567261;
             }
         }
         
