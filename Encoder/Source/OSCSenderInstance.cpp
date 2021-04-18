dbd3063a03bc2b85a9b97d91ea0b78b853972846
Christian Schweizer <christian@schweizerweb.ch>
2021-04-18 15:57:25 +0200

    parser improved

--- Encoder/Source/OSCSenderInstance.cpp
+++ Encoder/Source/OSCSenderInstance.cpp
@@ -86,63 +86,69 @@ bool OSCSenderInstance::analyzeString(std::string parameterString, Array<UserDef
     {
         auto fullString = parameterMatch[0].str();
         auto userString = parameterMatch[1].str();
-        switch(userString.substr()[0])
+        if(userString.length() == 1)
         {
-            case 'a': pArray->add(UserDefinedParameter(fullString, ParameterType::A)); break;
-            case 'e': pArray->add(UserDefinedParameter(fullString, ParameterType::E)); break;
-            case 'd': pArray->add(UserDefinedParameter(fullString, ParameterType::D)); break;
-            case 'x': pArray->add(UserDefinedParameter(fullString, ParameterType::X)); break;
-            case 'y': pArray->add(UserDefinedParameter(fullString, ParameterType::Y)); break;
-            case 'z': pArray->add(UserDefinedParameter(fullString, ParameterType::Z)); break;
-            case 'i': pArray->add(UserDefinedParameter(fullString, ParameterType::Index)); break;
-            case 'n': pArray->add(UserDefinedParameter(fullString, ParameterType::Name)); break;
-            case 'c': pArray->add(UserDefinedParameter(fullString, ParameterType::Color)); break;
-            case 'g': pArray->add(UserDefinedParameter(fullString, ParameterType::Gain)); break;
-            case 's':
-                if(userString.length() == 2)
+            switch(userString.substr()[0])
+            {
+                case 'a': pArray->add(UserDefinedParameter(fullString, ParameterType::A)); break;
+                case 'e': pArray->add(UserDefinedParameter(fullString, ParameterType::E)); break;
+                case 'd': pArray->add(UserDefinedParameter(fullString, ParameterType::D)); break;
+                case 'x': pArray->add(UserDefinedParameter(fullString, ParameterType::X)); break;
+                case 'y': pArray->add(UserDefinedParameter(fullString, ParameterType::Y)); break;
+                case 'z': pArray->add(UserDefinedParameter(fullString, ParameterType::Z)); break;
+                case 'i': pArray->add(UserDefinedParameter(fullString, ParameterType::Index)); break;
+                case 'n': pArray->add(UserDefinedParameter(fullString, ParameterType::Name)); break;
+                case 'c': pArray->add(UserDefinedParameter(fullString, ParameterType::Color)); break;
+                case 'g': pArray->add(UserDefinedParameter(fullString, ParameterType::Gain)); break;
+            }
+        }
+        else if(userString.substr()[0]== 's')
+        {
+            if(userString.length() == 2)
+            {
+                switch(userString.substr()[1])
                 {
-                    switch(userString.substr()[1])
-                    {
-                        case 'a': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledA)); break;
-                        case 'e': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledE)); break;
-                        case 'd': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledD)); break;
-                        case 'x': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledX)); break;
-                        case 'y': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledY)); break;
-                        case 'z': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledZ)); break;
+                    case 'a': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledA)); break;
+                    case 'e': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledE)); break;
+                    case 'd': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledD)); break;
+                    case 'x': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledX)); break;
+                    case 'y': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledY)); break;
+                    case 'z': pArray->add(UserDefinedParameter(fullString, ParameterType::ScaledZ)); break;
                         default: return false;
-                    }
+                }
+            }
+            else
+            {
+                std::smatch sm;
+                        
+                if(regex_search(userString, sm, rThreeValues))
+                {
+                    double lo = std::stod(sm[2]);
+                    double zero = std::stod(sm[3]);
+                    double hi = std::stod(sm[4]);
+                    if(lo == hi && lo == zero)
+                        return false;
+                            
+                    pArray->add(UserDefinedParameter(fullString, sm[1].str(), lo, hi, zero));
+                }
+                else if(regex_search(userString, sm, rTwoValues))
+                {
+                    double lo = std::stod(sm[2]);
+                    double hi = std::stod(sm[3]);
+                    if(lo == hi)
+                        return false;
+                        
+                    pArray->add(UserDefinedParameter(fullString, sm[1].str(), lo, hi));
                 }
                 else
                 {
-                    std::smatch sm;
-                        
-                    if(regex_search(userString, sm, rThreeValues))
-                    {
-                        double lo = std::stod(sm[2]);
-                        double zero = std::stod(sm[3]);
-                        double hi = std::stod(sm[4]);
-                        if(lo == hi && lo == zero)
-                            return false;
-                            
-                        pArray->add(UserDefinedParameter(fullString, sm[1].str(), lo, hi, zero));
-                    }
-                    else if(regex_search(userString, sm, rTwoValues))
-                    {
-                        double lo = std::stod(sm[2]);
-                        double hi = std::stod(sm[3]);
-                        if(lo == hi)
-                            return false;
-                            
-                        pArray->add(UserDefinedParameter(fullString, sm[1].str(), lo, hi));
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return false;
                 }
-                break;
-            default:
-                return false;
+            }
+        }
+        else
+        {
+            return false;
         }
         
         parameterString = parameterMatch.suffix();
