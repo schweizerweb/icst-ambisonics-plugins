c3beaea17920b6ca3ff0cc15627c1699eb86c8a8
Christian Schweizer <christian@schweizerweb.ch>
2023-09-18 23:59:33 +0200

    file headers added

--- Encoder/Source/CustomOscInput.cpp
+++ Encoder/Source/CustomOscInput.cpp
@@ -1,12 +1,23 @@
 /*
-  ==============================================================================
+================================================================================
+    This file is part of the ICST AmbiPlugins.
 
-    CustomOscInput.cpp
-    Created: 17 Jan 2022 10:20:47pm
-    Author:  Schweizer Christian
+    ICST AmbiPlugins are free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-  ==============================================================================
+    ICST AmbiPlugins are distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the ICSTAmbiPlugins.  If not, see <http://www.gnu.org/licenses/>.
+================================================================================
 */
+
+
 
 #include "CustomOscInput.h"
 
