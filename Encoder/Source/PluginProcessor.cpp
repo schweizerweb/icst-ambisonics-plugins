7be39345d5057fb6f56c582b8c053cac158477a3
Christian Schweizer <christian@schweizerweb.ch>
2023-10-09 09:47:44 +0200

    limit processblock to number of channels in buffer

--- Encoder/Source/PluginProcessor.cpp
+++ Encoder/Source/PluginProcessor.cpp
@@ -228,7 +228,7 @@ void AmbisonicEncoderAudioProcessor::processBlock (AudioSampleBuffer& buffer, Mi
 {
 	// Audio handling
     const float masterGainFactor = float(Decibels::decibelsToGain(sources->getMasterGain()));
-	const int totalNumInputChannels = jmin(getTotalNumInputChannels(), sources->size());
+	const int totalNumInputChannels = jmin(getTotalNumInputChannels(), sources->size(), buffer.getNumChannels());
 	const int totalUsedOutputChannels = jmin(getTotalNumOutputChannels(), encoderSettings.getAmbiChannelCount(), buffer.getNumChannels());
 	double currentCoefficients[64];
 	float* outputBufferPointers[64];
