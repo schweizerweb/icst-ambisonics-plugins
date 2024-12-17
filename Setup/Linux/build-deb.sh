9eddcc9810ef3d7293f0b49e81489ae66a28c05e
Christian Schweizer <christian@schweizerweb.ch>
2024-12-17 21:26:22 +0100

    linux build updated

--- Setup/Linux/build-deb.sh
+++ Setup/Linux/build-deb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir -p build/.dpkg/DEBIAN
-mkdir -p build/.dpkg/tmp/icst.tmp/vst3
-mkdir -p build/.dpkg/tmp/icst.tmp/lv2
-mkdir -p build/.dpkg/tmp/icst.tmp/templates
-cp -r build/bin/VST3/* build/.dpkg/tmp/icst.tmp/vst3 || exit -1;
-cp -r build/bin/LV2/* build/.dpkg/tmp/icst.tmp/lv2 || exit -1;
-cp -r Setup/Templates/* build/.dpkg/tmp/icst.tmp/templates || exit -1
+mkdir -p build/.dpkg/usr/lib/vst3
+mkdir -p build/.dpkg/usr/lib/lv2
+mkdir -p build/.dpkg/usr/lib/icst.templates
+cp -r build/bin/VST3/* build/.dpkg/usr/lib/vst3 || exit -1;
+cp -r build/bin/LV2/* build/.dpkg/usr/lib/lv2 || exit -1;
+cp -r Setup/Templates/* build/.dpkg/usr/lib/icst.templates || exit -1
 
 cp $(dirname "$0")/postinst build/.dpkg/DEBIAN/ || exit -1;
 chmod +x build/.dpkg/DEBIAN/postinst
@@ -27,7 +27,7 @@ Priority: optional
 Architecture: all
 Maintainer: ICST <christian.schweizer@zhdk.ch>
 Homepage: http://www.ambisonics.ch
-License: GPL-3.0
+License: GPL-3
 Description: $DESCRIPTION
 EOF
 
